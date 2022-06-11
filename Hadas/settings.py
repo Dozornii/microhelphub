a989e67b94013e4117e2cb19a16d792ea87123aa
unknown <dozornii@gmail.com>
2022-06-11 11:18:30 +0300

    Initial commit3

--- Hadas/settings.py
+++ Hadas/settings.py
@@ -163,4 +163,4 @@ import dj_database_url
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
 
-ALLOWED_HOSTS = ['herokudjangoapp.herokuapp.com']
+ALLOWED_HOSTS = ['hadas2.herokuapp.com']
